bd651a821ee4d04bd1b9fae11366c00cb0b603c7
Aurélien Leygues <aurelien.leygues@gmail.com>
2022-04-28 10:46:54 +0200

    upding orm config

--- super-api/src/index.ts
+++ super-api/src/index.ts
@@ -10,16 +10,35 @@ const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
     // database connection, the config is loaded from ormconfig.json
-    await createConnection();
+    await createConnection({
+        "type": "mysql",
+        "host": process.env.MYSQL_HOST || "db",
+        "port": process.env.MYSQL_PORT ? Number(process.env.MYSQL_PORT) : 3306,
+        "username": process.env.MYSQL_USER || "root",
+        "password": process.env.MYSQL_PASSWORD || "supersecure",
+        "database": process.env.MYSQL_DATABASE || "wcs",
+        "synchronize": true,
+        "logging": true,
+        "entities": [
+            "src/models/**/*.ts"
+        ],
+        "migrations": [
+            "src/migrations/**/*.ts"
+        ],
+        "cli": {
+            "entitiesDir": "src/models",
+            "migrationsDir": "src/migrations"
+        }
+    });
 
     // ... Building schema here
     const schema = await buildSchema({
         resolvers: [UsersResolver, ProjectsResolver],
         authChecker: customAuthChecker,
-    });  
+    });
 
     // Create the GraphQL server
-    const server = new ApolloServer({ 
+    const server = new ApolloServer({
         schema,
         context: ({ req }) => {
             return {
